c20d1ff167d86ebbe24c93faa641509eacc56feb
Jeff Irwin <jeff.irwin@live.com>
2017-07-25 16:09:40 -0500

    updated app.js and index files to read css from public

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const data = require('./data.js');
+const app = express();
 
 app.engine('mustache', mustacheExpress());
 
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('index', data);
+});
+
+app.listen(3000, function() {
+  console.log('Listening to port 3000');
+});
