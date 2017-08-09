538f62f4d1b8424deae9961d8ce3449839466fb5
Jeff Irwin <jeff.irwin@live.com>
2017-08-09 16:07:22 -0500

    initial changes - moving towards mongoose

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-// const routes = require('./routes/users.js');
-const mongoClient = require('mongodb').MongoClient();
+const mongoose = require('mongoose');
+
+/****** MONGOOSE SETUP  ******/
+
+mongoose.connect('mongodb://localhost:27017/robots', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+/****** APP SETUP ******/
 
 const app = express();
+
+/******  MUSTACHE SETUP  ******/
 
 let mustacheInstance = mustacheExpress();
 mustacheInstance.cache = null;
@@ -12,59 +20,12 @@ app.engine('mustache', mustacheInstance);
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+/****** STATIC FILES  ******/
+
 app.use(express.static('public'));
 
-// app.use('/', routes);
+/****** APP.LISTEN  ******/
 
-express.response.debugRender = function(template, data) {
-  data.json = JSON.stringify(data, null, '/t');
-  this.render(template, data);
-};
-
-const url = 'mongodb://localhost:27017/robots';
-
-app.get('/', function(req, res) {
-  db.collection('users')
-  .find({})
-  .toArray(function(err, results) {
-    res.render('index', { users: results });
-  })
-});
-
-app.get('/profile/:id', function(req, res) {
-  db.collection('users')
-  .find({ id: Number(req.params.id) })
-  .toArray(function(err, results) {
-    res.render('profile', results[0] );
-  })
-});
-
-app.get('/forHire', function(req, res) {
-  db.collection('users')
-  .find({ job: { $type: 10 } })
-  .toArray(function(err, results) {
-    res.render('index', { users: results });
-  })
-});
-
-app.get('/workingRobots', function(req, res) {
-  db.collection('users')
-  .find({ job: { $type: 2 } })
-  .toArray(function(err, results) {
-    res.render('index', { users: results })
-  })
-});
-
-let db;
-
-mongoClient.connect(url, function(err, database) {
-  if (err) {
-    console.error(err);
-  }
-  else {
-    db = database;
-    app.listen(3000, function() {
-      console.log('Listening to port 3000');
-    });
-  }
+app.listen(3000, function() {
+  console.log('Listening to port 3000');
 });
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,35 @@
 const router = require('express').Router();
+
+router.get('/', function(req, res) {
+  db.collection('users')
+  .find({})
+  .toArray(function(err, results) {
+    res.render('index', { users: results });
+  })
+});
+
+router.get('/profile/:id', function(req, res) {
+  db.collection('users')
+  .find({ id: Number(req.params.id) })
+  .toArray(function(err, results) {
+    res.render('profile', results[0] );
+  })
+});
+
+router.get('/forHire', function(req, res) {
+  db.collection('users')
+  .find({ job: { $type: 10 } })
+  .toArray(function(err, results) {
+    res.render('index', { users: results });
+  })
+});
+
+router.get('/workingRobots', function(req, res) {
+  db.collection('users')
+  .find({ job: { $type: 2 } })
+  .toArray(function(err, results) {
+    res.render('index', { users: results })
+  })
+});
+
+module.exports = router;
