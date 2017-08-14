f2a1b64c61e1bba20e8e1439eaed2a4370e0c804
Jeff Irwin <jeff.irwin@live.com>
2017-08-14 08:33:05 -0500

    added styling and fixed profile update

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ function authRequired(req, res, next) {
 }
 
 router.get('/users', authRequired, function(req, res) {
-  console.log('the user?', req.user);
+  // console.log('the user?', req.user);
   User.find({}).then(function(results) {
     res.render('users', { users: results });
   });
@@ -28,12 +28,41 @@ router.get('/profile/:id', authRequired, function(req, res) {
   });
 });
 
-router.get('/profile/edit/:id', (req, res) => {
+router.get('/profile/edit/:id', authRequired, (req, res) => {
   let robotId = req.params.id;
 
   User.findById(robotId).then(results => {
     res.render('profileedit', results);
   })
+});
+
+router.post('/profile/edit/:id', authRequired, (req, res) => {
+  let robotId = req.params.id;
+
+  console.log(req.body);
+
+  User.findByIdAndUpdate(robotId, { $set: req.body}, function(err, results) {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(results);
+    }
+  });
+  res.redirect('/users');
+});
+
+router.post('/profile/delete/:id', authRequired, (req, res) => {
+  let robotid = req.params.id;
+
+  User.findByIdAndRemove(robotid, (err, results) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      res.redirect('/users');
+    }
+  });
 });
 
 router.get('/forHire', authRequired, function(req, res) {
